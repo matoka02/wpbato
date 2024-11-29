ac7fd5f6eaa957bcdcff9b7ae81f3f2602f74a5a
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-11-29 14:40:53 +0200

    add header

--- header.php
+++ header.php
@@ -16,20 +16,38 @@
 </head>
 
 <body <?php body_class(); ?>>
-<header>
-	<div class="container">
+<header  class="page-header">
+	<div class="container  header-container">
 		<a href="<?php echo home_url(); ?>" class="logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="logo">
+			<picture>
+				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@2x.png 2x"
+								media="(min-width: 768px)" type="image/png">
+				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@2x.png 2x"
+								media="(max-width: 768px)"
+								type="image/png">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png" alt="logo">
+			</picture>
+<!--			<img src="--><?php //echo get_template_directory_uri(); ?><!--/assets/images/logo.svg" alt="logo">-->
 		</a>
 
-		<?php
-		wp_nav_menu( [
-			'theme_location' => 'header_menu',
-			'menu_id' => 'header-menu',
-			'container' => 'nav',
-			'container_class' => 'header-menu',
-			'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-		]);
-		?>
+		<div class="navigation">
+			<?php
+			wp_nav_menu( [
+				'theme_location' => 'header_menu',
+				'menu_id' => 'header-menu',
+				'container' => 'nav',
+				'container_class' => 'header-menu',
+				'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			]);
+			?>
+
+			<a href="tel:+0596763818" class="nav__phone">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone.svg" alt="number_phone">
+				0596 76 38 18
+			</a>
+		</div>
+
+
+
 	</div>
 </header>
