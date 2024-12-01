8d679848acb573885eb0ad9e0b9a4f1ae3c62499
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-12-01 18:50:54 +0200

    add features

--- header.php
+++ header.php
@@ -6,48 +6,50 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> dir="ltr">
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="format-detection" content="telephone=no">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-		<title>AIE</title>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<title>AIE</title>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<header  class="page-header">
-	<div class="container  header-container">
+	<header class="page-header">
+		<div class="container  header-container">
 
-		<a href="<?php echo home_url(); ?>" class="logo">
-			<picture>
-				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@2x.png 2x"
-								media="(min-width: 768px)" type="image/png">
-				<source srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@2x.png 2x"
-								media="(max-width: 768px)"
-								type="image/png">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png" alt="logo">
-			</picture>
-		</a>
-
-		<div class="navigation">
-			<?php
-			wp_nav_menu( [
-				'theme_location' => 'header_menu',
-				'menu_id' => 'header-menu',
-				'container' => 'nav',
-				'container_class' => 'header-menu',
-				'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-			]);
-			?>
-
-			<a href="tel:+0596763818" class="nav__phone">
-				<svg width="14" height="14">
-					<use href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#phone"></use>
-				</svg>
-				0596 76 38 18
+			<a href="<?php echo home_url(); ?>" class="logo">
+				<picture>
+					<source
+						srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-desktop-@2x.png 2x"
+						media="(min-width: 768px)" type="image/png">
+					<source
+						srcset="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png 1x, <?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@2x.png 2x"
+						media="(max-width: 768px)" type="image/png">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-AIE1-mobile-@1x.png" alt="logo">
+				</picture>
 			</a>
-		</div>
 
-	</div>
-</header>
+			<div class="navigation">
+				<?php
+				wp_nav_menu([
+					'theme_location' => 'header_menu',
+					'menu_id' => 'header-menu',
+					'container' => 'nav',
+					'container_class' => 'header-menu',
+					'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				]);
+				?>
+
+				<a href="tel:+0596763818" class="nav__phone">
+					<svg width="14" height="14">
+						<use href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#phone"></use>
+					</svg>
+					0596 76 38 18
+				</a>
+			</div>
+
+		</div>
+	</header>
--- template-parts/home-page-features.php
+++ template-parts/home-page-features.php
@@ -11,40 +11,36 @@ $features_list = get_field('features_example');
 ?>
 
 
-<section class="features">
-  <div class="container features__wrapper">
-    <!-- Title -->
-    <?php if ($title): ?>
-      <h2 class="title"><?php echo esc_html($title); ?></h2>
-    <?php endif; ?>
-
-    <!-- Description -->
-    <?php if ($description): ?>
-      <p class="title__label"><?php echo esc_html($description); ?></p>
-    <?php endif; ?>
-
-    <!-- Features List -->
-    <?php if ($features_list): ?>
-      <ul class="features__list">
-        <?php foreach ($features_list as $feature): ?>
+<section class='features'>
+  <div class='container features__wrapper'>
+    <h2 class='title'><?php echo esc_html( $title ); ?></h2>
+    <p class='title__label'><?php echo esc_html( $description ); ?></p>
+    <ul class='features__list'>
+      <?php if( $features_list ): ?>
+        <?php 
+          // Limit the number of elements to 3
+          $features_list = array_slice( $features_list, 0, 3 );
+          // var_dump( $features_list );
+        ?>
+        <?php foreach( $features_list as $index => $feature ): ?>
           <li>
-            <!-- Feature Image -->
-            <?php if (!empty($feature['icon'])): ?>
-              <img src="<?php echo esc_url($feature['icon']); ?>" alt="symbol" class="features__img">
+            <?php 
+              // We get data for each element
+              $feature_image = $feature['features_example_image'];
+              $feature_image_url = isset($feature_image['url']) ? $feature_image['url'] : '';
+              $feature_title = $feature['features_example_title'];
+              $feature_description = $feature['features_example_description'];
+            ?>
+            <?php if( !empty( $feature_image_url ) ): ?>
+              <img src='<?php echo esc_url( $feature_image_url ); ?>' alt='symbol' class='features__img'>
             <?php endif; ?>
-
-            <!-- Feature Description -->
-            <div class="features__descr">
-              <?php if (!empty($feature['title'])): ?>
-                <p><?php echo esc_html($feature['title']); ?></p>
-              <?php endif; ?>
-              <?php if (!empty($feature['description'])): ?>
-                <p><?php echo esc_html($feature['description']); ?></p>
-              <?php endif; ?>
+            <div class='features__descr'>
+              <p><?php echo esc_html( $feature_title ); ?></p>
+              <p><?php echo esc_html( $feature_description ); ?></p>
             </div>
           </li>
         <?php endforeach; ?>
-      </ul>
-    <?php endif; ?>
+      <?php endif; ?>
+    </ul>
   </div>
 </section>
